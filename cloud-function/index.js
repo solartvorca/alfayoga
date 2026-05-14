80a38c6407d120f3a28e7cc007d2737b71c1f286
Wuallar <wuallar@gmail.com>
2026-05-14 17:02:12 +0300

    Fix CORS settings for Cloud Run

--- cloud-function/index.js
+++ cloud-function/index.js
@@ -4,7 +4,11 @@ const cors = require('cors');
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'OPTIONS'],
+    allowedHeaders: ['Content-Type']
+}));
 
 // Настройки Gmail (используй переменные окружения)
 const transporter = nodemailer.createTransport({
